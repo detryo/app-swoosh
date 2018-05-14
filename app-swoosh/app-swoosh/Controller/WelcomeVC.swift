1ba5260cca74d53f983901602ce10d15fdee5ce0
Cristian Sedano <cristiansedanoarenas@MacBook-Pro-de-Cristian.local>
2018-05-14 16:24:24 +0100

    Implement Method and Model
    Implement Model (Player.swift), and functions buttons for League.swift, and
    passing data between ViewControllers

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeVC: UIViewController {
     
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
@@ -19,6 +19,9 @@ class ViewController: UIViewController {
         
         
     }
-    
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
+        
+    }
 }
 
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Cristian Sedano on 14/5/18.
+//  Copyright © 2018 Cristian Sedano. All rights reserved.
+//
+
+import Foundation
